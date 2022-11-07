c6a3b3e67dbb44cac37235947c8b0826d4dcfbf2
QuantumBreak3678 <68030853+juandagongoradiaz@users.noreply.github.com>
2022-11-06 23:46:57 -0500

    Correciones finales

---
+++ js/main.js
@@ -0,0 +1,33 @@
+function changeOpacity2d() {
+    var image1 = document.getElementsByClassName("artist2d");
+    image1.classList.add("op");
+}
+function changeOpacity3d() {
+    var image2 = document.getElementsByClassName("artist3d");
+    image2.classList.add("op");
+}
+function changeOpacityconcep() {
+    var image3 = document.getElementsByClassName("conceptartist");
+    image3.classList.add("op");
+}
+function changeOpacityui() {
+    var image4 = document.getElementsByClassName("uiux");
+    image4.classList.add("op");
+}
+function changeOpacityprog() {
+    var image5 = document.getElementsByClassName("prog");
+    image5.classList.add("op");
+}
+function changeOpacitysound() {
+    var image6 = document.getElementsByClassName("sound");
+    image6.classList.add("op");
+}
+function changeOpacityex() {
+    var image7 = document.getElementsByClassName("exint");
+    image7.classList.add("op");
+}
+function changeOpacitydes() {
+    var image8 = document.getElementsByClassName("gamedes");
+    image8.classList.add("op");
+
+}
